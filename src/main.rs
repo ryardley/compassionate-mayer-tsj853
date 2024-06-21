5052859442f6a4770383340bb2ed5beb2350e18b
ryardley <contact@rudiyardley.com>
2024-06-21 17:08:43 +1000

    Refactor to a keypair construction

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@
 //     sync::Arc,
 // };
 
-use std::{ops::{Add, Mul}, sync::Arc};
+use std::{
+    ops::{Add, Mul},
+    sync::Arc,
+};
 
 use anyhow::*;
 use fhe::bfv::{self, BfvParameters, Ciphertext, Encoding, Plaintext, PublicKey, SecretKey};
@@ -11,18 +14,30 @@ use fhe_traits::{FheDecoder, FheDecrypter, FheEncoder, FheEncrypter};
 use rand::thread_rng;
 
 // Define a kind of trait for a keypair that encrypts to Ct
-trait Encryptor {
+trait Keypair {
     fn encrypt(&self, value: u64) -> Result<Ciphertext>;
     fn decrypt(&self, ciphertext: &Ciphertext) -> Result<u64>;
 }
 
-struct FheEncryptor {
+struct FheKeypair {
     sk: SecretKey,
-    pk: PublicKey,
-    params: Arc<BfvParameters>,
+    pub pk: PublicKey,
+    pub params: Arc<BfvParameters>,
 }
 
-impl Encryptor for FheEncryptor {
+impl FheKeypair {
+    // might want to pass in rng...
+    pub fn generate(params: Arc<BfvParameters>) -> FheKeypair {
+        let mut rng = thread_rng();
+
+        let sk = SecretKey::random(&params, &mut rng);
+        let pk = PublicKey::new(&sk, &mut rng);
+
+        FheKeypair { sk, pk, params }
+    }
+}
+
+impl Keypair for FheKeypair {
     fn encrypt(&self, value: u64) -> Result<Ciphertext> {
         let mut rng = thread_rng();
         let input1: Vec<u64> = vec![value];
@@ -37,6 +52,10 @@ impl Encryptor for FheEncryptor {
     }
 }
 
+// 1. Also need an algorhythm on the server that takes various ciphertexts as input and operates on
+//    them
+// 2.
+
 fn main() -> Result<()> {
     let params = bfv::BfvParametersBuilder::new()
         .set_degree(2048)
@@ -44,30 +63,20 @@ fn main() -> Result<()> {
         .set_plaintext_modulus(1 << 9)
         .build_arc()?;
 
-    let mut rng = thread_rng();
-    let sk = SecretKey::random(&params, &mut rng);
-    let pk = PublicKey::new(&sk, &mut rng);
+    let keypair = FheKeypair::generate(params);
 
-    let input1: Vec<u64> = vec![2];
-    let input2: Vec<u64> = vec![4];
-    let input3: Vec<u64> = vec![10];
+    let inputs = [2,4,10];
 
-    let pt1 = Plaintext::try_encode(&input1, Encoding::poly(), &params)?;
-    let pt2 = Plaintext::try_encode(&input2, Encoding::poly(), &params)?;
-    let pt3 = Plaintext::try_encode(&input3, Encoding::poly(), &params)?;
-
-    let ct1 = pk.try_encrypt(&pt1, &mut rng)?;
-    let ct2 = pk.try_encrypt(&pt2, &mut rng)?;
-    let ct3 = pk.try_encrypt(&pt3, &mut rng)?;
+    let ct1 = keypair.encrypt(inputs[0])?;
+    let ct2 = keypair.encrypt(inputs[1])?;
+    let ct3 = keypair.encrypt(inputs[2])?;
 
     let mut prod = &ct1 * &ct2;
     prod = &prod * &ct3;
 
-    let decrypted = sk.try_decrypt(&prod)?;
-    let tally_vec = Vec::<u64>::try_decode(&decrypted, Encoding::poly())?;
-    let tally_result = tally_vec[0];
+    let tally_result = keypair.decrypt(&prod)?;
 
-    let expected_result: u64 = input1[0] * input2[0] * input3[0];
+    let expected_result: u64 = inputs[0] * inputs[1] * inputs[2];
 
     println!("\nExpected Result: {:?}", expected_result);
     println!("Decrypted Result: {:?}\n", tally_result);
