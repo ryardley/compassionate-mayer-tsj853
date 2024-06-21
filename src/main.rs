45369b11abf2b54b2ba46fa2a460957e9f0309d8
ryardley <contact@rudiyardley.com>
2024-06-21 16:47:18 +1000

    Getting stuck on some typings

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,43 @@
-use std::ops::{Add, Mul};
+// use std::{
+//     ops::{Add, Mul},
+//     sync::Arc,
+// };
 
-use fhe::bfv::{self, Encoding, Plaintext, PublicKey, SecretKey};
+use std::{ops::{Add, Mul}, sync::Arc};
+
+use anyhow::*;
+use fhe::bfv::{self, BfvParameters, Ciphertext, Encoding, Plaintext, PublicKey, SecretKey};
 use fhe_traits::{FheDecoder, FheDecrypter, FheEncoder, FheEncrypter};
 use rand::thread_rng;
-use anyhow::*;
 
+// Define a kind of trait for a keypair that encrypts to Ct
+trait Encryptor<Ct>
+where
+    Ct: Add<Output = &Ct> + Mul<Output = &Ct>,
+{
+    fn encrypt<T>(&self, value: T) -> Result<Ct>;
+    fn decrypt<T>(&self, ciphertext: Ct) -> Result<T>;
+}
 
-// Define a kind of trait for a keypair that encrypts to Ct 
-trait Encryptor<Ct> where Ct: Add<Output = Ct> + Mul<Output = Ct> {
-    fn encrypt() -> Result<Ct>;  
-    fn decrypt(ciphertext:Ct) -> Result<u64>;
+struct FheEncryptor {
+    sk: SecretKey,
+    pk: PublicKey,
+    params: Arc<BfvParameters>,
+}
+
+impl Encryptor<Ciphertext> for FheEncryptor {
+    fn encrypt(&self, value: u64) -> Result<Ciphertext> {
+        let mut rng = thread_rng();
+        let input1: Vec<u64> = vec![value];
+        let pt1 = Plaintext::try_encode(&input1, Encoding::poly(), &self.params)?;
+        Ok(self.pk.try_encrypt(&pt1, &mut rng)?)
+    }
+
+    fn decrypt(&self, ciphertext: Ciphertext) -> Result<u64> {
+        let decrypted = self.sk.try_decrypt(&ciphertext)?;
+        let decrypted_vec = Vec::<u64>::try_decode(&decrypted, Encoding::poly())?;
+        Ok(decrypted_vec[0])
+    }
 }
 
 fn main() -> Result<()> {
